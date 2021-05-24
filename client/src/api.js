f40f63fce97475c670a281df888ee6433b975336
avsssai <34810623+avsssai@users.noreply.github.com>
2021-05-25 03:54:37 +0530

    frontend predeploy commit

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-	baseURL: "http://localhost:5000",
+	baseURL: process.env.REACT_APP_BACKEND_API || "http://localhost:5000",
 });
 
 API.interceptors.request.use((req) => {
